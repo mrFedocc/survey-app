95f9f31ec5e9620f8107aa807b5d9fadc500140c
VladimirMalevanik <deektorm@gmail.com>
2025-09-14 15:15:14 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,5 @@
+export const metadata = {
+  title: 'Petly – умный ошейник для питомцев',
+  description: 'Предзаказ умного GPS-ошейника Petly с мониторингом активности и здоровья питомца',
+  // ...другие мета-теги (og:image, twitter:image, итд)
+};
