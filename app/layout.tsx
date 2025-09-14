04de577c5300de5badda8af569fb604e8fc66866
VladimirMalevanik <deektorm@gmail.com>
2025-09-14 15:21:22 +0300

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,5 +0,0 @@
-export const metadata = {
-  title: 'Petly – умный ошейник для питомцев',
-  description: 'Предзаказ умного GPS-ошейника Petly с мониторингом активности и здоровья питомца',
-  // ...другие мета-теги (og:image, twitter:image, итд)
-};
