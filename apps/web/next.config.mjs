c61692c4cb777d4fb8bdfc32234bc36d5450c987
mrFedocc <232109740+mrFedocc@users.noreply.github.com>
2025-09-21 01:49:22 +0300

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Собираем статический сайт (под GitHub Pages)
   output: 'export',
-
-  // Куда класть готовый статики
   distDir: 'out',
-
-  // Если используешь внешние картинки — добавь нужные домены.
-  images: {
-    unoptimized: true
-  }
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
